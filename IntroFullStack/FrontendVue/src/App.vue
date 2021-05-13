2b1503a83ac82479f54ff8c1fdf9f8fdf22e7a52
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-05-13 18:13:22 -0300

    Aulas configurando prettier finalizada 😎

--- IntroFullStack/FrontendVue/src/App.vue
+++ IntroFullStack/FrontendVue/src/App.vue
@@ -1,8 +1,12 @@
 <template>
+  <!--É aqui que ficam os códigos HTML com Vue:-->
+
   <h1>Hello World :D</h1>
 </template>
 
 <script lang="ts">
+//É aqui que ficam os códigos CSS com Vue:
+
 import { defineComponent } from "vue";
 
 export default defineComponent({
