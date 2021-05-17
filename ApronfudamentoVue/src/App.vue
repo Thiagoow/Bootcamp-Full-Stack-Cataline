43a112624c8be7ad9a4d4a632107ca712df8cc84
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-05-17 15:33:48 -0300

    Projeto criado com sucesso :)

--- ApronfudamentoVue/src/App.vue
+++ ApronfudamentoVue/src/App.vue
@@ -1,27 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <h1>Helloツ</h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 
-export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-});
+export default defineComponent({});
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
