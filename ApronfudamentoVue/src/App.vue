373a4aebeb16d46c1ccffb4af013660975573336
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-05-17 16:32:52 -0300

    Aula computed concluída :D

--- ApronfudamentoVue/src/App.vue
+++ ApronfudamentoVue/src/App.vue
@@ -1,9 +1,37 @@
 <template>
-  <h1>Helloツ</h1>
+  <h1>Helloツ {{ person.name }}</h1>
+  <h2>Your full name is: {{ fullName }}</h2>
+  <!--Test-->
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
-export default defineComponent({});
+interface Person {
+  name: string;
+  surname: string;
+  age: number;
+}
+export default defineComponent({
+  data() {
+    return {
+      person: {
+        name: "Thiago",
+        surname: "Silva Lopes",
+        age: 18,
+      } as Person,
+    };
+  },
+  //Propriedade computada - retorna um dado com algum tipo de alteração:
+  computed: {
+    fullName(): string {
+      return `${this.person.name} ${this.person.surname}`;
+      //Nesse caso, nossa alteração é concatenar o nome e sobrenome numa função :)
+    },
+  },
+});
 </script>
+
+<style scoped>
+/*Teste*/
+</style>
