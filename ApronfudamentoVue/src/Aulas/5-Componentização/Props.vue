7edbd2b76bfb31f7a18ae5f2846529001928c9f4
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-05-24 16:07:55 -0300

    Quase finalizada a aula de Props 😁

--- ApronfudamentoVue/src/Aulas/5-Componentização/Props.vue
+++ ApronfudamentoVue/src/Aulas/5-Componentização/Props.vue
@@ -3,11 +3,12 @@
     <PropsButtonStyled text="Cadastro" color="primary" />
     <!--Um prop é uma propriedade/atributo, que vamos passar para um elemento. Nesse caso,
   temos dois botões criados com o mesmo componente (PropsButtonStyled), mas cada um deles irá
-  realizar uma função diferente. Para isso atribuímos props diferentes pra cada um. */
-  
-  /*Sendo necessário, também, que tais propriedades sejam definidas 
-  no componente pai, o qual foi importado.-->
-    <PropsButtonStyled text="Login" color="primary" />
+  realizar uma função diferente. Para isso atribuímos props diferentes pra cada um. Sendo necessário, 
+  também, que tais propriedades sejam definidas no componente pai, o qual foi importado.-->
+
+    <PropsButtonStyled text="Login" color="danger" />
+    <!--Botão mas com props diferentes do acima, que usa o mesmo componente, mas esse contém uma classe dinâmica
+    "button-styled" e a classe estática "color", que tem o valor padrão/default: "primary"-->
   </div>
 </template>
 
--- ApronfudamentoVue/src/components/PropsButtonStyled.vue
+++ ApronfudamentoVue/src/components/PropsButtonStyled.vue
@@ -7,6 +7,7 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
+//Cria a tipagem de tipo color usada no "validator:", no objeto props, aqui embaixo:
 type color = "success" | "danger" | "primary";
 
 export default defineComponent({
@@ -19,9 +20,13 @@ export default defineComponent({
       //Se a prop é obrigatória:
       required: true,
     },
+    //Tal componente pode ter a propriedade:
     color: {
+      //Com o tipo:
       type: String,
+      //Com o valor padrão:
       default: "primary",
+      //Aceita apenas os valores que puxamos de: "color":
       validator: (value: color) => {
         return ["success", "danger", "primary"].includes(value);
       },
@@ -44,13 +49,23 @@ export default defineComponent({
 
 .success {
   background-color: green;
+  /*Adiciona um efeito de glow: */
+  box-shadow: 0 0 5px 3px rgba(76, 234, 45, 0.3);
 }
 
 .danger {
   background-color: red;
+  /*Adiciona um efeito de glow: */
+  box-shadow: 0 0 5px 3px rgba(234, 45, 45, 0.3);
 }
+/*Como estamos usando o css global na pasta "public",
+a classe dinâmica primary fica obtem as propriedades do css global.
+Agora caso você não esteja utilizando o css global, a dynamicClass primary
+teria as props:
 
 .primary {
   background-color: #2d6cea;
+  box-shadow: 0 0 5px 3px rgba(45, 108, 234, 0.3);
 }
+*/
 </style>
