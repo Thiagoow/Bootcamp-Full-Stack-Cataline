a7fd0db972798e3aabc4759a9b574f22e4ea5669
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-08-27 16:01:41 -0300

    Na aula de Definindo Model :D

--- IntroAdonis/database/migrations/1630088009932_posts.ts
+++ IntroAdonis/database/migrations/1630088009932_posts.ts
@@ -1,27 +1,36 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
-/* Comandos do Adonis pra database:
-  UNDO/Desfazer: "node ace migration:rollback"
-*/
+/* Isso é uma migration :D É com elas que podemos inserir, alterar
+e deletar (CRUD) os dados e tabelas da nossa dB conectada :D */
+
 export default class Posts extends BaseSchema {
   protected tableName = 'posts'
 
   /* Esse método 'up' cria uma tabela: 
   Usando o comando -> "node ace migration:run" no terminal: */
   public async up() {
-    //Comando o qual cria uma tabela:
+    //Comando o qual cria uma tabela com as colunas:
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('title')
-      table.text('content', 'longtext') //text -> String maior
+      //notNullable() -> Não pode ser vazio:
+      table.string('title').notNullable()
+      //text -> String maior:
+      table.text('content', 'longtext').notNullable()
       table.timestamps(true) /*Cria um 'created_at' e um 'updated_at' os 
       quais são atualizados automaticamente */
     })
   }
 
-  /* Esse método abaixo 'down' destrói/delete uma tabela:
-  Usando o comando -> "" no terminal: */
+  /* Esse método abaixo 'down' destrói/delete uma
+  tabela INTEIRA E TODO SEU CONTEÚDO:
+  Usando o comando -> "node ace migration:rollback" no terminal: */
   public async down() {
     this.schema.dropTable(this.tableName)
   }
+
+  /* Criando uma nova coluna na tabela sem precisar deletar
+  ela INTEIRA e TODO seu (Criando uma outra/nova migration):
+
+  "node ace make:migration nomeMigration --table=nomeTabela"
+  */
 }
