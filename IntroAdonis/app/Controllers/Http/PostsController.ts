c07d230e616f56ae3352e621be24b7f152a80931
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-08-27 17:06:06 -0300

    Operações de CRUD

--- IntroAdonis/app/Controllers/Http/PostsController.ts
+++
@@ -1,9 +0,0 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
-export default class PostsController {
-  // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
-  index() {
-    return ['postagem 1', 'postagem 2', 'postagem 3']
-  }
-}
-//Route -> Controller -> Model(Opcionalmente)
---
+++ IntroAdonis/app/Models/Post.ts
@@ -0,0 +1,21 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Post extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public title: string
+
+  //Na dB essa coluna se chama 'content':
+  @column({ columnName: 'content' })
+  //Mas aqui prefiro chama-la de 'body':
+  public body: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
