983b7dc15db3320c916c9b02c6da8e5d6c32f217
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-09-01 15:42:54 -0300

    Agora retornando respostas em camelCase invés de snake_case :D

--- IntroAdonis/config/database.ts
+++ IntroAdonis/config/database.ts
@@ -11,29 +11,29 @@ import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
 
 const databaseConfig: DatabaseConfig & { orm: Partial<OrmConfig> } = {
   /*
-   |--------------------------------------------------------------------------
-   | Connection
-   |--------------------------------------------------------------------------
-   |
-   | The primary connection for making database queries across the application
-   | You can use any key from the `connections` object defined in this same
-   | file.
-   |
-   */
+    |--------------------------------------------------------------------------
+    | Connection
+    |--------------------------------------------------------------------------
+    |
+    | The primary connection for making database queries across the application
+    | You can use any key from the `connections` object defined in this same
+    | file.
+    |
+    */
   connection: Env.get('DB_CONNECTION'),
 
   connections: {
     /*
-     |--------------------------------------------------------------------------
-     | MySQL config
-     |--------------------------------------------------------------------------
-     |
-     | Configuration for MySQL database. Make sure to install the driver
-     | from npm when using this connection
-     |
-     | npm i mysql
-     |
-     */
+      |--------------------------------------------------------------------------
+      | MySQL config
+      |--------------------------------------------------------------------------
+      |
+      | Configuration for MySQL database. Make sure to install the driver
+      | from npm when using this connection
+      |
+      | npm i mysql
+      |
+      */
     mysql: {
       client: 'mysql',
       connection: {
@@ -49,24 +49,18 @@ const databaseConfig: DatabaseConfig & { orm: Partial<OrmConfig> } = {
   },
 
   /*
-   |--------------------------------------------------------------------------
-   | ORM Configuration
-   |--------------------------------------------------------------------------
-   |
-   | Following are some of the configuration options to tweak the conventional
-   | settings of the ORM. For example:
-   |
-   | - Define a custom function to compute the default table name for a given model.
-   | - Or define a custom function to compute the primary key for a given model.
-   |
-   */
-  orm: {
-    /* Para que as requisições retornem as 
-    colunas como respostas em sintaxe camelCase: */
-    getSerializeAsKey(_, key) {
-      return key
-    },
-  },
+    |--------------------------------------------------------------------------
+    | ORM Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Following are some of the configuration options to tweak the conventional
+    | settings of the ORM. For example:
+    |
+    | - Define a custom function to compute the default table name for a given model.
+    | - Or define a custom function to compute the primary key for a given model.
+    |
+    */
+  orm: {},
 }
 
 export default databaseConfig
