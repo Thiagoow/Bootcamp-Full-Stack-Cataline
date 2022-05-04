e8b232f99487bb81b871b70a8ebdf5490a208326
Thiago Silva Lopes <71602458+Thiagoow@users.noreply.github.com>
2022-05-04 01:39:20 -0300

    Example enhanced

--- IntroTypescript/src/Tipos/TypeAliases.ts
+++ IntroTypescript/src/Tipos/TypeAliases.ts
@@ -4,13 +4,18 @@ TypeAliases --> Tipo e apelido.
 
 Ao invés de colocar todos os animais como
 parâmetros da função pets, criamos o type,
-e definimos os apelidos:
-*/
+e definimos os apelidos: */
 
 type Pet = "dog" | "cat" | "hamster" | "turtle";
-
+//Useful in other functions that have the same Params
 function ShowPet(pet: Pet) {
   console.log(pet);
 }
+
+/* Way better than define like:
+function ShowPet(pet: "dog" | "cat" | "hamster" | "turtle") {
+  console.log(pet);
+}
+*/
 
 ShowPet("cat");
